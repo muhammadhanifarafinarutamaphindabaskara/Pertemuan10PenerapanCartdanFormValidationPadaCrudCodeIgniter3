6cfcfc2f2c647bc9814cee046a91f6976d996e6d
muhammadhanifarafinarutamaphindabaskara <rafiaja441@gmail.com>
2021-11-18 22:27:10 +0700

    Pertemuan 10_V3920038_Muhammad Hanif Arafi_V3920042_Narutama Phinda Baskara_Praktik Pemrograman Backend_TI-E

---
+++ application/views/footer.php
@@ -0,0 +1,32 @@
+<!-- Footer ================================================================== -->
+<div id="footerSection">
+	<div class="container">
+		<div class="row">
+			<div class="span3">
+				<h5>Tentang Kami</h5>
+				<p>SIMWA merupakan Sistem Informasi Manajemen Mahasiswa, dimana selain sistem manajemen, Kami juga Menjual Berbagai Perlengkapan sekolah yang lengkap</p>
+			</div>
+			<div class="span3">
+				<h5>Menu</h5>
+				<a href="<?php echo site_url('produk/kategori/Buku'); ?>">Buku</a>
+				<a href="<?php echo site_url('produk/kategori/Pulpen'); ?>">Pulpen</a>
+				<a href="<?php echo site_url('produk/kategori/Pensil'); ?>">Pensil</a>
+				<a href="<?php echo site_url('produk/kategori/Kotak_Pensil'); ?>">Kotak Pensil</a>
+				<a href="<?php echo site_url('produk/kategori/Tas'); ?>">Tas</a>
+			</div>
+		</div>
+		<p class="pull-right">&copy; Copyright 2021 SIMWA</p>
+	</div><!-- Container End -->
+</div>
+<!-- Placed at the end of the document so the pages load faster ============================================= -->
+<script src="<?php echo base_url('assets/bootshop/themes/js/jquery.js'); ?>" type="text/javascript"></script>
+<script src="<?php echo base_url('assets/bootshop/themes/js/bootstrap.min.js'); ?>" type="text/javascript"></script>
+<script src="<?php echo base_url('assets/bootshop/themes/js/google-code-prettify/prettify.js'); ?>"></script>
+
+<script src="<?php echo base_url('assets/bootshop/themes/js/bootshop.js'); ?>"></script>
+<script src="<?php echo base_url('assets/bootshop/themes/js/jquery.lightbox-0.5.js'); ?>"></script>
+
+<!-- Themes switcher section ============================================================================================= -->
+</body>
+
+</html>
